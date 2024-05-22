025ea8af30e0a67478f754d4f1bb5c4ba9f0893b
Dylan Mikus <dbmikus@users.noreply.github.com>
2024-05-22 12:41:55 -0400

    Add a simple memory summarization implementation inspired by MemGPT (#15)
    Add a simple memory summarization implementation, inspired by MemGPT.
    The `MemGPTSummaryAgent` keeps track of all historical messages, and
    whenever the message history exceeds the context window, it summarizes
    the history and swaps in the summary.
    
    The `MemGPTSummaryAgent` still needs a little work. For example, we
    should be able to keep some system configuration messages outside of
    message summary so that they are always present and are not mixed into
    the summary.

---
+++ tests/memory/test_memgpt.py
@@ -0,0 +1,66 @@
+from typing import List
+
+from openai.types.chat.chat_completion_message_param import ChatCompletionMessageParam
+from openai.types.chat import ChatCompletion
+
+from fixpoint.memory import WithMemory
+from fixpoint.memory._memgpt import (
+    MemGPTSummaryAgent,
+    MemGPTSummarizeOpts,
+    _ContextLengthCheck,
+)
+from fixpoint.agents.mock import MockAgent, new_mock_completion
+from fixpoint.utils.messages import smsg, umsg
+
+
+class TestMemGPTSummaryAgent:
+    def test_simple_create_completion(self) -> None:
+        mem = WithMemory()
+        agent = MockAgent(completion_fn=_completion_fn, memory=mem)
+        summary_opts = MemGPTSummarizeOpts(agent=agent, context_window=200)
+        summary_agent = MemGPTSummaryAgent(summary_opts)
+
+        compl = summary_agent.create_completion(
+            messages=[
+                smsg(
+                    "You are a friendly AI customer support agent. You chat with a user to answer their questions"
+                ),
+                umsg("What is your name?"),
+            ]
+        )
+
+        # We are under the token limit, so we don't summarize.
+        assert len(mem.memory()) == 1
+        # We are under the token limit, so we did not summarize and product any
+        # extra messages.
+        assert len(mem.memory()[0][0]) == 2
+
+
+def test_context_length_check() -> None:
+    agent = MockAgent(
+        completion_fn=_completion_fn,
+    )
+    summary_opts = MemGPTSummarizeOpts(agent=agent, context_window=200)
+    messages: List[ChatCompletionMessageParam] = []
+    extra_messages: List[ChatCompletionMessageParam] = [
+        smsg(
+            "You are a friendly AI customer support agent. You chat with a user to answer their questions"
+        ),
+        umsg("What is your name?"),
+    ]
+    lencheck = _ContextLengthCheck.from_opts_and_messages(
+        opts=summary_opts,
+        messages=messages,
+        extra_preserved_messages=extra_messages,
+    )
+
+    # Make sure that the context length check does not modify either of the
+    # lists.
+    assert len(messages) == 0
+    assert len(extra_messages) == 2
+
+
+def _completion_fn() -> ChatCompletion:
+    return new_mock_completion(
+        "I am a friendly AI customer support agent here to assist you with any questions or issues you may have. How can I help you today?"
+    )
