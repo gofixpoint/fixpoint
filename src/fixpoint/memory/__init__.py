619c2fa7fe90c44928919499ebbc19ae78639c6f
Dylan Mikus <dbmikus@gmail.com>
2024-05-22 00:13:34 -0400

    Add a simple memory summarization implementation inspired by MemGPT
    Add a simple memory summarization implementation, inspired by MemGPT.
    The `MemGPTSummaryAgent` keeps track of all historical messages, and
    whenever the message history exceeds the context window, it summarizes
    the history and swaps in the summary.
    
    The `MemGPTSummaryAgent` still needs a little work. For example, we
    should be able to keep some system configuration messages outside of
    message summary so that they are always present and are not mixed into
    the summary.

--- src/fixpoint/memory/__init__.py
+++ src/fixpoint/memory/__init__.py
@@ -1,8 +1,12 @@
 """LLM agent memory"""
 
+from ._memgpt import MemGPTSummarizeOpts, MemGPTSummaryAgent, memgpt_summarize
 from ._memory import WithMemory, WithMemoryProto
 
 __all__ = [
+    "MemGPTSummarizeOpts",
+    "MemGPTSummaryAgent",
     "WithMemory",
     "WithMemoryProto",
+    "memgpt_summarize",
 ]
