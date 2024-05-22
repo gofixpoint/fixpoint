0b639647c8d26122526fab78d5d7b80f7affd1ab
Dylan Mikus <dbmikus@users.noreply.github.com>
2024-05-22 12:38:21 -0400

    Make tweaks to the OpenAI agent (#14)
    Make some tweaks to the OpenAI agent class:
    
    It inherits (and thus conforms to) the `BaseAgent` protocol. In doing
    so, we implemented the `count_tokens` method and the `create_completion`
    method.
    
    `create_completion` basically does the same thing as the the behavior of
    calling `OpenAI().chat.completions.create(...)` via the `__getattr__`
    passthrough. However, it does make requests to various pre and post
    processor functions, which the passthrough to the underlying OpenAI
    client does not do. We should reconcile that later.
    
    We also added some new features to the `OpenAI` agent, which are passed
    in during initialization and otherwise occur in the background.
    
    You can init it with `pre_completion_fns`, which are called before the
    inference request. They accept the messages as an arg, and return a
    modified messages list (or they can do something else).
    
    You can init the object with `completion_callbacks`, which are called
    after the inference request succeeds. They accept both the messages and
    the resulting completion.
    
    You can init the object `memory`, which maintains a memory of all past
    LLM calls.
    
    Also, squeeze in a couple fixes for the tests, making the
    `openai_test.py` use pytest assertions.

--- src/fixpoint/agents/openai.py
+++ src/fixpoint/agents/openai.py
@@ -3,20 +3,38 @@ This module contains the OpenAIAgent class, which is responsible for handling th
 interaction between the user and OpenAI.
 """
 
-from typing import Any, get_args
+from typing import Any, List, Optional, cast, get_args
 
 import openai
 import instructor
+from openai.types.chat.chat_completion_message_param import ChatCompletionMessageParam
+from openai.types.chat.chat_completion import ChatCompletion
+import tiktoken
 
 from .._utils.completions import decorate_instructor_completion_with_fixp
+from .protocol import BaseAgent, CompletionCallback, PreCompletionFn
+from ..memory import WithMemoryProto
 
 
-class OpenAIAgent:
+class OpenAIAgent(BaseAgent):
     """
     This is the Agent class.
     """
 
-    def __init__(self, model_name: str, api_key: str) -> None:
+    _client: instructor.Instructor
+    _completion_callbacks: List[CompletionCallback]
+    _pre_completion_fns: List[PreCompletionFn]
+    _memory: Optional[WithMemoryProto]
+
+    def __init__(
+        self,
+        model_name: str,
+        api_key: str,
+        *,
+        pre_completion_fns: Optional[List[PreCompletionFn]] = None,
+        completion_callbacks: Optional[List[CompletionCallback]] = None,
+        memory: Optional[WithMemoryProto] = None,
+    ) -> None:
         # if instance of models is not one of the supported models, raise ValueError
         supported_models = get_args(openai.types.ChatModel)
         if model_name not in supported_models:
@@ -39,6 +57,63 @@ class OpenAIAgent:
 
         setattr(self._client.chat.completions, "create", decorated_method)
 
+        self._completion_callbacks = completion_callbacks or []
+        self._pre_completion_fns = pre_completion_fns or []
+        self._memory = memory
+
     def __getattr__(self, name: str) -> Any:
         # Forward attribute access to the underlying client
         return getattr(self._client, name)
+
+    def create_completion(
+        self,
+        *,
+        messages: List[ChatCompletionMessageParam],
+        **kwargs: Any,
+    ) -> ChatCompletion:
+        """Create a completion"""
+        if "stream" in kwargs and kwargs["stream"]:
+            raise ValueError("Streaming is not supported yet.")
+
+        messages = self._trigger_pre_completion_fns(messages)
+
+        completion = self._client.chat.completions.create(
+            messages=messages,
+            model=self.model_name,
+            # TODO(dbmikus) support streaming mode.
+            stream=False,
+            **kwargs,
+        )
+        # For some reason, the type checker doesn't understand that the type of
+        # completion is ChatCompletion.
+        # Note, when we support streaming, the return type will be different
+        # depending on whether streaming is on or off.
+        fixed_completion = cast(ChatCompletion, completion)
+
+        if self._memory is not None:
+            self._memory.store_memory(messages, fixed_completion)
+        self._trigger_completion_callbacks(messages, fixed_completion)
+        return fixed_completion
+
+    def count_tokens(self, s: str) -> int:
+        """Count the tokens in the string, according to the model's agent(s)"""
+        encoding = tiktoken.encoding_for_model(self.model_name)
+        return len(encoding.encode(s))
+
+    def _trigger_pre_completion_fns(
+        self, messages: List[ChatCompletionMessageParam]
+    ) -> List[ChatCompletionMessageParam]:
+        """Trigger the pre-completion functions"""
+        for fn in self._pre_completion_fns:
+            messages = fn(messages)
+        return messages
+
+    # TODO(dbmikus) this does not work when we call
+    # `self.chat.completions.create` because that is a blind pass-through call
+    # to the OpenAI client class, so we have no way to control it.
+    def _trigger_completion_callbacks(
+        self, messages: List[ChatCompletionMessageParam], completion: ChatCompletion
+    ) -> None:
+        """Trigger the completion callbacks"""
+        for callback in self._completion_callbacks:
+            callback(messages, completion)
