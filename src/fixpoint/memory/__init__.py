6e8b2ad2219b0e2cc2b7f2fe51270206f1784a7c
Dylan Mikus <dbmikus@users.noreply.github.com>
2024-05-22 12:37:30 -0400

    Add a simple memory protocol and class (#11)
    Add a simple memory protocol that dictates the basic interface for
    acting on agent memory.
    
    Add a `WithMemory` class that acts as memory storage. The user is
    supposed to use this as a mix-in to an `BaseAgent` implementation, which
    can then make use of memory. Although, technically they could also
    interact directly with the memory class.

---
+++ src/fixpoint/memory/__init__.py
@@ -0,0 +1,8 @@
+"""LLM agent memory"""
+
+from ._memory import WithMemory, WithMemoryProto
+
+__all__ = [
+    "WithMemory",
+    "WithMemoryProto",
+]
---
+++ src/fixpoint/memory/_memory.py
@@ -0,0 +1,76 @@
+"""Code for agent memory"""
+
+from typing import List, Protocol, Tuple
+
+
+from ..chat import ChatCompletionMessageParam, ChatCompletion
+
+
+class WithMemoryProto(Protocol):
+    """A protocol for adding memory to an agent"""
+
+    def memory(self) -> List[Tuple[List[ChatCompletionMessageParam], ChatCompletion]]:
+        """Get the memory"""
+
+    def store_memory(
+        self, messages: List[ChatCompletionMessageParam], completion: ChatCompletion
+    ) -> None:
+        """Store the memory"""
+
+    def format_str(self) -> str:
+        """Return the formatted string of messages. Useful for printing/debugging"""
+
+
+class WithMemory(WithMemoryProto):
+    """A composable class to add memory to an agent"""
+
+    _memory: List[Tuple[List[ChatCompletionMessageParam], ChatCompletion]]
+
+    def __init__(self) -> None:
+        self._memory = []
+
+    def callback(
+        self, messages: List[ChatCompletionMessageParam], completion: ChatCompletion
+    ) -> None:
+        """Handle completions with memory context"""
+        self.store_memory(messages, completion)
+
+    def store_memory(
+        self, messages: List[ChatCompletionMessageParam], completion: ChatCompletion
+    ) -> None:
+        """Store the memory
+
+        Args:
+            messages (List[ChatCompletionMessageParam]): List of message parameters.
+            completion (Optional[ChatCompletion]): The completion object, if any.
+        """
+        self._memory.append((messages, completion))
+
+    def memory(self) -> List[Tuple[List[ChatCompletionMessageParam], ChatCompletion]]:
+        """Get the memory"""
+        return self._memory
+
+    def format_str(self) -> str:
+        """Return the formatted string of messages. Useful for printing/debugging"""
+        delim = "============================================================"
+        lines = []
+        for mem in self.memory():
+            lines.extend(self._format_single_mem(mem[0], mem[1]))
+            lines.append(delim)
+        return "\n".join(lines)
+
+    def _format_single_mem(
+        self, messages: List[ChatCompletionMessageParam], completion: ChatCompletion
+    ) -> List[str]:
+        """Return the formatted string of a single memory entry"""
+        lines = [f'{m["role"]}: {m["content"]}' for m in messages]
+        lines.append(f"assistant: {completion.choices[0].message.content}")
+        return lines
+
+
+# Check that we implement the protocol
+def _check(_c: WithMemoryProto) -> None:
+    pass
+
+
+_check(WithMemory())
