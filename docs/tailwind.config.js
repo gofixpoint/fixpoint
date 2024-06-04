6e8ce5d133f7e3ff5812c2d39140f5e95667071e
Dylan Mikus <dbmikus@gmail.com>
2024-06-04 16:21:20 -0400

    [docs] Set up the docs for Fixpoint
    Set up the docs for Fixpoint:
    
    - Remove most of the Nextra docs template stuff
    - Update all links to our Github, Discord, homepage, etc.
    - Update package.json metadata
    - Add Tailwind CSS support

---
+++ docs/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,md,mdx}", "./theme.config.tsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
