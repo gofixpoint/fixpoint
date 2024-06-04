21674277d6d9296ebdd3f594f307c728c4807ed3
Jakub Cichon <jakub@fixpoint.co>
2024-06-04 17:33:55 -0400

    Add storage support to memory. Test with supabase

--- src/fixpoint/memory/_memory.py
+++ src/fixpoint/memory/_memory.py
@@ -1,7 +1,7 @@
 """Code for agent memory"""
 
-from dataclasses import dataclass
-from typing import List, Protocol, Optional
+from typing import List, Protocol, Optional, Any, Callable
+import json
 
 from pydantic import BaseModel
 
@@ -10,13 +10,43 @@ from ..workflow import SupportsWorkflow
 from ..storage.protocol import SupportsStorage
 
 
-@dataclass
 class MemoryItem:
     """A single memory item"""
 
     messages: List[ChatCompletionMessageParam]
     completion: ChatCompletion[BaseModel]
     workflow: Optional[SupportsWorkflow] = None
+
+    def __init__(
+        self,
+        messages: List[ChatCompletionMessageParam],
+        completion: ChatCompletion[BaseModel],
+        workflow: Optional[SupportsWorkflow] = None,
+        serialize_fn: Callable[[Any], str] = json.dumps,
+        deserialize_fn: Callable[[str], Any] = json.loads,
+    ) -> None:
+        self.messages = messages
+        self.completion = completion
+        self.workflow = workflow
+        self._serialize_fn = serialize_fn
+        self._deserialize_fn = deserialize_fn
+
+    def serialize(self) -> dict[str, Any]:
+        """Convert the item to a dictionary"""
+        return {
+            "messages": self._serialize_fn(self.messages),
+            "completion": self.completion.serialize_json(),
+            "workflow": self._serialize_fn(self.workflow),
+        }
+
+    @classmethod
+    def deserialize(cls, data: dict[str, Any]) -> "MemoryItem":
+        """Deserialize a dictionary into a TLRUCacheItem"""
+
+        messages = json.loads(data.pop("messages"))
+        completion = ChatCompletion[BaseModel].deserialize_json(data.pop("completion"))
+        workflow = json.loads(data.pop("workflow"))
+        return cls(messages=messages, completion=completion, workflow=workflow)
 
 
 class SupportsMemory(Protocol):
@@ -59,7 +89,9 @@ class Memory(SupportsMemory):
             messages (List[ChatCompletionMessageParam]): List of message parameters.
             completion (Optional[ChatCompletion]): The completion object, if any.
         """
-        mem_item = MemoryItem(messages=messages, completion=completion, workflow=workflow)
+        mem_item = MemoryItem(
+            messages=messages, completion=completion, workflow=workflow
+        )
         self._memory.append(mem_item)
         if self._storage is not None:
             self._storage.insert(mem_item)
--- tests/memory/test_memory.py
+++ tests/memory/test_memory.py
@@ -1,9 +1,11 @@
 from typing import List, Tuple
+import json
 import pytest
 from fixpoint.completions import ChatCompletionMessageParam
 from fixpoint.memory import Memory, MemoryItem
 from fixpoint.agents.mock import new_mock_completion
 from fixpoint.storage.supabase import SupabaseStorage
+from ..supabase_test_utils import test_inputs
 
 
 class TestWithMemory:
@@ -15,7 +17,18 @@ class TestWithMemory:
         ]
         cmpl = new_mock_completion()
         memstore.store_memory(msgs, cmpl)
-        assert memstore.memory() == [MemoryItem(messages=msgs, completion=cmpl)]
+
+        stored_memory = memstore.memory()
+        expected_memory = [MemoryItem(messages=msgs, completion=cmpl)]
+
+        assert len(stored_memory) == len(expected_memory) == 1
+
+        first_stored_memory = stored_memory[0]
+        first_expected_memory = expected_memory[0]
+        assert first_stored_memory.messages == first_expected_memory.messages
+        assert first_stored_memory.completion == first_expected_memory.completion
+        assert first_stored_memory.workflow == first_expected_memory.workflow
+
 
 @pytest.mark.skip(reason="Disabled until we have a supabase instance running in CI")
 class TestWithMemoryWithStorage:
@@ -28,22 +41,22 @@ class TestWithMemoryWithStorage:
         CREATE TABLE IF NOT EXISTS public.memory_store (
             messages jsonb PRIMARY KEY,
             completion jsonb,
-            workflow jsonb,
+            workflow jsonb
         );
 
-        TRUNCATE TABLE public.memory_store;
+        TRUNCATE TABLE public.memory_store
         """,
                 "public.memory_store",
             )
         ],
         indirect=True,
     )
-    def test_store_memory_with_storage(self,  test_inputs: Tuple[str, str]) -> None:
+    def test_store_memory_with_storage(self, test_inputs: Tuple[str, str]) -> None:
         url, key = test_inputs
         storage = SupabaseStorage(
             url,
             key,
-            table="memory_state",
+            table="memory_store",
             order_key="messages",
             id_column="messages",
             value_type=MemoryItem,
@@ -56,5 +69,23 @@ class TestWithMemoryWithStorage:
         ]
         cmpl = new_mock_completion()
         memstore.store_memory(msgs, cmpl)
-        assert memstore.memory() == [MemoryItem(messages=msgs, completion=cmpl)]
 
+        stored_memory = memstore.memory()
+        expected_memory = [MemoryItem(messages=msgs, completion=cmpl)]
+
+        assert len(stored_memory) == len(expected_memory) == 1
+
+        first_stored_memory = stored_memory[0]
+        first_expected_memory = expected_memory[0]
+
+        # The objects aren't the same address anymore, so we can't just compare them
+        assert json.dumps(first_stored_memory.messages) == json.dumps(
+            first_expected_memory.messages
+        )
+        assert (
+            first_stored_memory.completion.serialize_json()
+            == first_expected_memory.completion.serialize_json()
+        )
+        assert json.dumps(first_stored_memory.workflow) == json.dumps(
+            first_expected_memory.workflow
+        )
