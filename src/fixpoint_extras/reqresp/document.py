026a5229bf30ca76352439c1c672507f18627750
Dylan Mikus <dbmikus@gmail.com>
2024-06-06 00:15:37 -0400

    more wip

--- src/fixpoint_extras/reqresp/document.py
+++ src/fixpoint_extras/reqresp/document.py
@@ -3,6 +3,12 @@ from typing import Dict, Any, Optional
 
 from pydantic import BaseModel, PrivateAttr, Field
 
+
+class Version(BaseModel):
+    num: int = Field(description="The version number of the document. Increases monotonically")
+    path: str = Field(description="For this version, the path to the task and step where we updated the document")
+
+
 class Document(BaseModel):
     """A document is a collection of text and metadata."""
 
@@ -17,8 +23,29 @@ class Document(BaseModel):
         ),
     )
 
+    path: str = Field(default="/", description="The path to the document in the workflow")
+
     @computed_field  # type: ignore[misc]
     @property
     def id(self) -> str:
         """The workflow's unique identifier"""
         return self._id
+
+    @computed_field  # type: ignore[misc]
+    @property
+    def task(self) -> str:
+        """The workflow's unique identifier"""
+        parts = self.path.split("/")
+        if len(parts) == 1:
+            return "__start__"
+        return parts[1]
+
+
+    @computed_field  # type: ignore[misc]
+    @property
+    def step(self) -> str:
+        """The workflow's unique identifier"""
+        parts = self.path.split("/")
+        if len(parts) < 3:
+            return "__start__"
+        return parts[2]
--- src/fixpoint_extras/reqresp/workflow.py
+++ src/fixpoint_extras/reqresp/workflow.py
@@ -1,11 +1,27 @@
 """Simple implementation of a workflow"""
 
 from uuid import uuid4
-from typing import List, Optional
+from typing import Any, Dict, List, Optional, Union
 
 from pydantic import BaseModel, Field, PrivateAttr, computed_field
 
 from .document import Document
+
+
+class NodeState(BaseModel):
+    """
+    Each task or step in a workflow is a "node". This keeps track of which node
+    the workflow is in.
+    """
+
+    task: str = Field(description="The task that the node is in")
+    step: str = Field(description="The step that the node is in")
+
+    @computed_field  # type: ignore[misc]
+    @property
+    def id(self) -> str:
+        """The workflow's unique identifier"""
+        return f"/{self.task}/{self.step}"
 
 
 class Workflow(BaseModel):
@@ -21,6 +37,8 @@ class Workflow(BaseModel):
     _id: str = PrivateAttr(default_factory=lambda: str(uuid4()))
     _run_id: str = PrivateAttr(default_factory=lambda: str(uuid4()))
     _task_ids: List[str] = PrivateAttr(default_factory=list)
+
+    node_state: NodeState = Field(default_factory=lambda: NodeState(task="__start__", step="__start__"))
 
     name: Optional[str] = Field(
         default=None,
@@ -42,17 +60,15 @@ class Workflow(BaseModel):
         """The workflow's unique identifier"""
         return self._run_id
 
-    def goto_task(self, *, task_id: Optional[str], task_name: Optional[str]) -> None:
+    def goto_task(self, *, task_name: str) -> None:
         """Transition to the given task.
-
-        Specify one of either task_id or task_name.
 
         Tasks do not need to be declared ahead of time. When you go to a task,
         we infer its existence.
         """
         ...
 
-    def goto_step(self, *, step_id: Optional[str], step_name: Optional[str]) -> None:
+    def goto_step(self, *, step_name: str) -> None:
         """Transition to the given step.
 
         Specify one of either step_id or step_name.
@@ -65,7 +81,28 @@ class Workflow(BaseModel):
     def get_document(self, *, document_id: Optional[str], document_name: Optional[str]) -> Document:
         """Get a document from the cache.
 
-        Specify one of either document_id or document_name. Gets the version of
-        the document for the current workflow, task, and step.
+        Specify one of either document_id or document_name. Gets the latest
+        version of the document for the current workflow, task, and step.
         """
-        ...
+        raise NotImplementedError()
+
+    def store_document(self, *, contents: Union[BaseModel, Dict[str, Any]], name: Optional[str] = None, path: Optional[str] = None) -> Document:
+        """Store a document in the cache.
+
+        If name is provided, the document will be stored under that name. If a
+        document with that name already exists in the workflow, we will throw an
+        error.
+
+        The optional `path` is a "/" separate path of the form "/{task}/{step}".
+        The "{step}" portion is optional. If you only specify the leading "/",
+        it is stored at the root of the workflow, outside of all tasks and
+        steps. By default, we store the document at the current task and step.
+        """
+        raise NotImplementedError()
+
+    def update_document(self, *, document_id: Optional[str], document_name: Optional[str], contents: Union[BaseModel, Dict[str, Any]]) -> Document:
+        """Update a document in the cache.
+
+        Specify one of either document_id or document_name.
+        """
+        raise NotImplementedError()
