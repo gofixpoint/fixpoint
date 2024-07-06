465fa5b11b86f297063c97d443e9482f803e275e
Dylan Mikus <dbmikus@gmail.com>
2024-07-05 22:44:51 -0400

    Added async agents

--- src/fixpoint/agents/__init__.py
+++ src/fixpoint/agents/__init__.py
@@ -2,9 +2,17 @@
 This is the agents module.
 """
 
-from .protocol import BaseAgent
-from .openai import OpenAIAgent
+__all__ = [
+    "AsyncBaseAgent",
+    "BaseAgent",
+    "AsyncOpenAIAgent",
+    "OpenAIAgent",
+    "CacheMode",
+    "oai",
+    "random_agent_id",
+]
+
+from .protocol import BaseAgent, AsyncBaseAgent
+from .openai import OpenAIAgent, AsyncOpenAIAgent
 from ._shared import CacheMode, random_agent_id
 from . import oai
-
-__all__ = ["BaseAgent", "OpenAIAgent", "CacheMode", "oai", "random_agent_id"]
