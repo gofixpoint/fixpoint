d5c356623fed02b9d7eb1cc1d0d451bcd6c4561c
Dylan Mikus <dbmikus@gmail.com>
2024-07-05 23:09:36 -0400

    Add async OpenAI API-compatible agent

--- src/fixpoint/agents/oai/_async_openai.py
+++ src/fixpoint/agents/oai/_async_openai.py
@@ -1,11 +1,12 @@
-"""A wrapper around the OpenAI client.
+"""A wrapper around the AsyncOpenAI client.
 
-A wrapper around the OpenAI client that gives it some extra features like
+A wrapper around the AsyncOpenAI client that gives it some extra features like
 caching, memory, pre-and-post inference callbacks, etc...
 """
 
-__all__ = ["OpenAI", "AsyncOpenAI"]
+__all__ = ["AsyncOpenAI"]
 
+import asyncio
 from typing import (
     Any,
     Iterable,
@@ -28,34 +29,33 @@ from fixpoint.completions import (
 from fixpoint.memory import SupportsMemory
 from fixpoint._protocols.workflow_run import WorkflowRunData
 from fixpoint.cache import SupportsChatCompletionCache
-from ..openai import OpenAIAgent, OpenAIClients
+from ..openai import AsyncOpenAIAgent, AsyncOpenAIClients
 from ..protocol import CompletionCallback, PreCompletionFn
-from ._async_openai import AsyncOpenAI
 
 
 T_contra = TypeVar("T_contra", bound=BaseModel, contravariant=True)
 
 
-class OpenAI:
+class AsyncOpenAI:
     """
     An agent that conforms to the OpenAI API.
     """
 
-    fixp: OpenAIAgent
-    _fixpchat: "OpenAI._Chat"
+    fixp: AsyncOpenAIAgent
+    _fixpchat: "AsyncOpenAI._Chat"
     _model_name: Optional[str]
 
     def __init__(
         self,
         agent_id: str,
-        openai_clients: OpenAIClients,
+        openai_clients: AsyncOpenAIClients,
         *,
         pre_completion_fns: Optional[List[PreCompletionFn]] = None,
         completion_callbacks: Optional[List[CompletionCallback]] = None,
         memory: Optional[SupportsMemory] = None,
         cache: Optional[SupportsChatCompletionCache] = None,
     ) -> None:
-        self.fixp = OpenAIAgent(
+        self.fixp = AsyncOpenAIAgent(
             agent_id=agent_id,
             model_name="<NOT_SET>",
             openai_clients=openai_clients,
@@ -65,10 +65,10 @@ class OpenAI:
             cache=cache,
         )
 
-        self._fixpchat = OpenAI._Chat(openai_clients.instructor.chat, self.fixp)
+        self._fixpchat = AsyncOpenAI._Chat(openai_clients.instructor.chat, self.fixp)
 
     @property
-    def chat(self) -> "OpenAI._Chat":
+    def chat(self) -> "AsyncOpenAI._Chat":
         """Chat-related operations"""
         return self._fixpchat
 
@@ -78,20 +78,20 @@ class OpenAI:
 
     class _Chat:
         _fixpchat: instructor.Instructor
-        _fixpcompletions: "OpenAI._Completions"
-        _agent: OpenAIAgent
+        _fixpcompletions: "AsyncOpenAI._Completions"
+        _agent: AsyncOpenAIAgent
 
         def __init__(
-            self, openai_chat: instructor.Instructor, agent: OpenAIAgent
+            self, openai_chat: instructor.Instructor, agent: AsyncOpenAIAgent
         ) -> None:
             self._fixpchat = openai_chat
-            self._fixpcompletions = OpenAI._Completions(
+            self._fixpcompletions = AsyncOpenAI._Completions(
                 self._fixpchat.completions, agent
             )
             self._agent = agent
 
         @property
-        def completions(self) -> "OpenAI._Completions":
+        def completions(self) -> "AsyncOpenAI._Completions":
             """Operations on chat completions"""
             return self._fixpcompletions
 
@@ -101,10 +101,10 @@ class OpenAI:
 
     class _Completions:
         _fixpcompletions: instructor.Instructor
-        _agent: OpenAIAgent
+        _agent: AsyncOpenAIAgent
 
         def __init__(
-            self, openai_completions: instructor.Instructor, agent: OpenAIAgent
+            self, openai_completions: instructor.Instructor, agent: AsyncOpenAIAgent
         ) -> None:
             self._fixpcompletions = openai_completions
             self._agent = agent
@@ -114,7 +114,7 @@ class OpenAI:
             return getattr(self._fixpcompletions, name)
 
         @overload
-        def create(
+        async def create(
             self,
             messages: List[ChatCompletionMessageParam],
             *,
@@ -128,7 +128,7 @@ class OpenAI:
         ) -> ChatCompletion[BaseModel]: ...
 
         @overload
-        def create(
+        async def create(
             self,
             messages: List[ChatCompletionMessageParam],
             *,
@@ -141,7 +141,7 @@ class OpenAI:
             **kwargs: Any,
         ) -> ChatCompletion[T_contra]: ...
 
-        def create(
+        async def create(
             self,
             messages: List[ChatCompletionMessageParam],
             *,
@@ -154,13 +154,17 @@ class OpenAI:
             **kwargs: Any,
         ) -> ChatCompletion[T_contra]:
             """Create a chat completion"""
-            return self._agent.create_completion(
-                messages=messages,
-                model=model,
-                temperature=temperature,
-                tool_choice=tool_choice,
-                tools=tools,
-                response_model=response_model,
-                workflow_run=workflow_run,
-                **kwargs,
-            )
+            async with asyncio.TaskGroup() as tg:
+                cmpl_t = tg.create_task(
+                    self._agent.create_completion(
+                        messages=messages,
+                        model=model,
+                        temperature=temperature,
+                        tool_choice=tool_choice,
+                        tools=tools,
+                        response_model=response_model,
+                        workflow_run=workflow_run,
+                        **kwargs,
+                    )
+                )
+            return cmpl_t.result()
